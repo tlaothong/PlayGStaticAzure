9113d5c48f88127335e15139b99a64f3feb66a93
Teerachai Laothong <tlaothong@gmail.com>
2023-07-08 15:06:04 +0700

    Moved back to src

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
